63f99ce7d4136591d629652c47738db0e879c77d
Matthew Paul <mattgpaul@gmail.com>
2024-04-17 17:16:10 -0600

    adjusted file names and tests

--- test/test_sec_data.py
+++ test/test_sec_data.py
@@ -1,14 +1,10 @@
 import pytest
-from unittest.mock import Mock
 from src.sec_data import SecData
 
-def mock_get(*args, **kwargs):
-    mock_response = Mock()
+def test_download_data(mocker):
+    mock_response = mocker.Mock()
     mock_response.content = b'PK\x03\x04\x14\x00\x00\x00\x08\x00n\xaeGP'
-    return mock_response
-
-def test_download_data(monkeypatch):
-    monkeypatch.setattr("requests.get", mock_get)
+    mocker.patch('requests.get', return_value=mock_response)
 
     sec_data = SecData()
     data = sec_data.download_data()
